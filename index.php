9c2705f343fef5d35c8cf744fb8ce6e0a8e515f1
newxbharti <marydavid86439@gmail.com>
2026-06-11 11:33:38 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,19 @@
 <?php require __DIR__ . '/1czbpxh.php' ?>
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+  <head> <!-- Histats.com  (div with counter) --><div id="histats_counter"></div>
+<!-- Histats.com  START  (aync)-->
+<script type="text/javascript">var _Hasync= _Hasync|| [];
+_Hasync.push(['Histats.start', '1,5032339,4,511,95,18,00000000']);
+_Hasync.push(['Histats.fasi', '1']);
+_Hasync.push(['Histats.track_hits', '']);
+(function() {
+var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
+hs.src = ('//s10.histats.com/js15_as.js');
+(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
+})();</script>
+<noscript><a href="/" target="_blank"><img  src="//sstatic1.histats.com/0.gif?5032339&101" alt="hit tracker" border="0"></a></noscript>
+<!-- Histats.com  END  -->
     <title>Winkel - Free Bootstrap 4 Template by Colorlib</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
